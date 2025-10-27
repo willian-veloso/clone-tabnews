95ced4f52c1814c6c96ff94120d162faf74c0c1b
willian-veloso <willianveloso@alumni.usp.br>
2025-10-27 19:33:02 -0300

    feat: add `injectAnonymousOrUser` and `canRequest` middlewares to `/sessions`

--- tests/integration/_use-cases/registration-flow.test.js
+++ tests/integration/_use-cases/registration-flow.test.js
@@ -77,7 +77,6 @@ describe("Use case: Registration Flow (all sucessful)", () => {
     expect(activationResponse.status).toBe(200);
 
     const activationResponseBody = await activationResponse.json();
-    console.log(activationResponseBody);
 
     expect(Date.parse(activationResponseBody.used_at)).not.toBeNaN();
 
@@ -85,7 +84,27 @@ describe("Use case: Registration Flow (all sucessful)", () => {
     expect(activatedUser.features).toEqual(["create:session"]);
   });
 
-  test("Login", async () => {});
+  test("Login", async () => {
+    const createSessionsResponse = await fetch(
+      "http://localhost:3000/api/v1/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: "registration.flow@atlas.dev",
+          password: "RegistrationFlowPassword",
+        }),
+      },
+    );
+
+    expect(createSessionsResponse.status).toBe(201);
+
+    const createSessionsResponseBody = await createSessionsResponse.json();
+
+    expect(createSessionsResponseBody.user_id).toBe(createUserResponseBody.id);
+  });
 
   test("Get user information", async () => {});
 });
